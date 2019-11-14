04b240aa21a52f11a69e32d7146698296f621d64
kllapuz3 <52308844+kllapuz3@users.noreply.github.com>
2019-11-14 14:44:52 +0800

    Add files via upload

--- postgresql_view_exercise_2tables_(kllapuz3)/bckp.sql
+++ postgresql_view_exercise_2tables_(kllapuz3)/bckp.sql
@@ -12,16 +12,6 @@ OWNER = klapuz;
 
 \c datama2
 
-CREATE TABLE commodities (
-	id serial CONSTRAINT commodities_pkey PRIMARY KEY,
-	com_num int NOT NULL,
-	com_type varchar(50) NOT NULL,
-	com_validity timestamp(6) NOT NULL,
-	com_quantity int NOT NULL,
-	com_brand varchar(50),
-	com_worth int NOT NULL
-);
-
 CREATE TABLE employee (
 	id serial CONSTRAINT employee_pkey PRIMARY KEY,
 	emp_num int NOT NULL,
@@ -32,20 +22,34 @@ CREATE TABLE employee (
 	emp_dob timestamp(6) NOT NULL	
 );
 
-INSERT INTO public.commodities(
-	id, com_num, com_type, com_validity, com_worth, com_quality, com_brand)
-	VALUES (1, 1, 'TypeA', '060821', 100, 50, 'BrandA');
+CREATE TABLE commodities (
+	id serial CONSTRAINT commodities_pkey PRIMARY KEY,
+	employee_id int NOT NULL,
+	com_num int NOT NULL,
+	com_type varchar(50) NOT NULL,
+	com_validity timestamp(6) NOT NULL,
+	com_quantity int NOT NULL,
+	com_brand varchar(50),
+	com_worth int NOT NULL,
+	FOREIGN KEY (employee_id) REFERENCES employee (id)
+);
 
-INSERT INTO public.commodities(
-	id, com_num, com_type, com_validity, com_worth, com_quality, com_brand)
-	VALUES (2, 2, 'TypeB', '060821', 99, 49, 'BrandB');
+INSERT INTO public.employee(id, emp_num, emp_fname, emp_lname, emp_initial, emp_address, emp_dob)
+VALUES 
+(1, 1, 'Steve', 'Cruz', 'K', 'Mandaluyong City', '211021'),
+(2, 2, 'Chris', 'Reyes', 'A', 'Pasay City', '231221');
 
-INSERT INTO public.employee(
-	id, emp_num, emp_fname, emp_lname, emp_initial, emp_address, emp_dob)
-	VALUES (1, 1, 'Steve', 'Cruz', 'K', 'Mandaluyong City', '211021');
+INSERT INTO public.commodities(id, employee_id, com_num, com_type, com_validity, com_worth, com_quantity, com_brand)
+VALUES
+(1, 2, 1, 'TypeA', '060821', 100, 50, 'BrandA'),
+(2, 1, 2, 'TypeB', '060821', 99, 49, 'BrandB');
 
-INSERT INTO public.employee(
-	id, emp_num, emp_fname, emp_lname, emp_initial, emp_address, emp_dob)
-	VALUES (2, 2, 'Chris', 'Reyes', 'A', 'Pasay City', '231221');
-
-
+CREATE VIEW employee_commo AS
+SELECT
+commodities.id,
+commodities.com_num,
+employee.emp_fname,
+employee.emp_lname
+FROM commodities
+JOIN employee ON (commodities.employee_id = employee.id)
+ORDER BY commodities.id;
